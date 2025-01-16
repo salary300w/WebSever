add2313068ecb9661151a1ec7790847c370b0006
huaweiyun <huaweiyun@huawei.com>
2025-01-16 18:00:01 +0800

    Thu Jan 16 06:00:01 PM CST 2025

--- root/StrategyC/articles.js
+++ root/StrategyC/articles.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_01_16", file: "2025_01_16.html", summary: "2025_01_16股票数据"},
     { title: "2025_01_15", file: "2025_01_15.html", summary: "2025_01_15股票数据"},
     { title: "2025_01_14", file: "2025_01_14.html", summary: "2025_01_14股票数据"},
     { title: "2025_01_13", file: "2025_01_13.html", summary: "2025_01_13股票数据"},
